f6a09f80ae35776df81fb1ff2cdab128d227f49e
asura349 <perfection8@naver.com>
2020-05-02 12:18:38 +0900

    Update 9_example.R

--- 9_example.R
+++ 9_example.R
@@ -10,9 +10,11 @@ VC~~ 1*VC
 VC~ 0*1
 VC=~ NA*s2+ s6+ s11+ s15'
 
-model.vc.cfa<- cfa(model.vc, data= vc.data, group= "age")
+fit.vc.cfa<- cfa(model.vc, data= vc.data, group= "age")
 
 samplesize<- c(232, 211, 207, 197, 202, 197, 196, 197, 193, 227, 198)
 
-vc.align.rst <- align.main(model.vc.cfa, 4, "VC", samplesize)
+# align.main function input ref.
+# align.main<- function(fit, num.item, factor, samplesize) {...}
+vc.align.rst <- align.main(fit.vc.cfa, 4, "VC", samplesize)
 vc.align.rst
