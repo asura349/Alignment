47f629051f041a2757657704e89cecf0191eb0c5
asura349 <perfection8@naver.com>
2020-05-02 12:13:59 +0900

    Update 3_align.main.R

--- 3_align.main.R
+++ 3_align.main.R
@@ -1,10 +1,11 @@
 align.main<- function(fit, num.item, factor, samplesize) {
     
-    # factor is factor name string.
     t0<- Sys.time()
     
     num.group<- length(samplesize)
     lambda0<- extract.lambda0(fit, num.item)
+    
+    # factor = factor name string.
     nu0<- extract.nu0(fit, num.item, factor)
     
     est.init<- c(rep(1, num.group), rep(0, (num.group-1)) )
@@ -32,19 +33,22 @@ align.main<- function(fit, num.item, factor, samplesize) {
     metric.int<- convert.metric.int(lambda0, nu0, metric.var, metric.mean)
     
     # aligned solution.
-    aligned.load.ref<- back.transf.load(metric.load, lambda0, metric.var, metric="refgroup")
-    aligned.int.fixed<- back.transf.int(metric.int, lambda0, nu0)
     aligned.var.ref<- back.transf.psi(lambda0, aligned.load.ref)
     aligned.mean.fixed<- back.transf.alpha(nu0, aligned.int.fixed, aligned.load.ref)
+    aligned.load.ref<- back.transf.load(metric.load, lambda0, metric.var, metric="refgroup")
+    aligned.int.fixed<- back.transf.int(metric.int, lambda0, nu0)    
     
     t9<- Sys.time()
     
-    # return result in listed form.
+    # return result in list form.
     result<- list(simplicity= rst$value,
+                  
                   metric.variance= metric.var, metric.mean= metric.mean,
                   metric.loading= metric.load, metric.intercept= metric.int,
+                  
                   aligned.loading.refgroup= aligned.load.ref, aligned.intercept.fixed= aligned.int.fixed,
                   aligned.variance.refgroup= aligned.var.ref, aligned.mean.fixed= aligned.mean.fixed,
+                  
                   opt.time= t2-t1, total.time= t9-t0)
     
     return(result)
